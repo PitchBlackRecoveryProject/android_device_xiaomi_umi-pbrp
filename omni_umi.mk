1fc775bee4a89a56c9b3f33c1ed8e21eae207412
MD Raza <raza231198@gmail.com>
2021-04-20 20:52:54 +0530

    umi: PitchBlack-ify

--- omni_umi.mk
+++ omni_umi.mk
@@ -18,10 +18,12 @@
 PRODUCT_RELEASE_NAME := umi
 DEVICE_PATH := device/xiaomi/umi
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from PitchBlack
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
